c2ae99479a7483abc4bde7aef14c9b422ccbeeb3
Coolertyo4 <lm.pramer@gmail.com>
2022-01-13 07:26:50 +0100

    Feature development

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -15,7 +15,6 @@
 
 #include "../constants.hpp"
 
-#include "head/controls.hpp"
 
 #include "../renderingHead/vboindexing.hpp"
 
@@ -23,19 +22,24 @@
 #include "../renderingHead/texture.hpp"
 #include "../renderingHead/objloader.hpp"
 
+#include "head/controls.hpp"
+#include "head/model.hpp"
+
 #define LOG
 
 #ifdef DEBUG
-#define lg printf("Log Point%d\n", i); i++
+#define lg printf("Log Point %d\n", i); i++
 int i = 0;
 #else
 #define lg
 #endif
 
 //the globally used variables
-GLFWwindow* window;
+GLFWwindow* window; //The window
 
-FILE* logFile;
+FILE* logFile; //The log file
+
+int error = 0; // the error codes
 
 GLuint LoadShaders(const char* vertex_file_path, const char* fragment_file_path);
 
@@ -50,31 +54,27 @@ void loadBuffers(
     std::vector<glm::vec3> indexed_vertices,
     std::vector<glm::vec2> indexed_uvs,
     std::vector<glm::vec3> indexed_normals);
+int render(Model model, glm::mat4 ProjectionMatrix, glm::mat4 ViewMatrix);
 
+static GLuint vertexbuffer;
+static GLuint uvbuffer;
+static GLuint normalbuffer;
+static GLuint elementbuffer;
 
+static GLuint VertexArrayID;
+static GLuint TextureID;
+static GLuint LightID;
 
-    
+static GLuint MatrixID;
+static GLuint ViewMatrixID;
+static GLuint ModelMatrixID;
 
 int Game(void)
 { 
-    int error = 0;
 
-    GLuint ProgramID;
+    GLuint program;
 
-    GLuint VertexArrayID;
-    GLuint TextureID;
-    GLuint LightID;
 
-    GLuint Texture;
-
-    GLuint vertexbuffer;
-    GLuint uvbuffer;
-    GLuint normalbuffer;
-    GLuint elementbuffer;
-
-    GLuint MatrixID;
-    GLuint ViewMatrixID;
-    GLuint ModelMatrixID;
 
     error = InitAll();
 
@@ -86,53 +86,50 @@ int Game(void)
     //Generate Vao
     GenVao(&VertexArrayID);
     
-    ProgramID = LoadShaders("src/renderingHead/Shaders/ShadingVertexShader.vs", "src/renderingHead/Shaders/ShadingFragmentShader.fs");
+    program = LoadShaders("src/renderingHead/Shaders/ShadingVertexShader.vs", "src/renderingHead/Shaders/ShadingFragmentShader.fs");
 
 
 
     // Get a handle for  "MVP" uniform
-	MatrixID = glGetUniformLocation(ProgramID, "MVP");
-    ViewMatrixID = glGetUniformLocation(ProgramID, "V");
-    ModelMatrixID = glGetUniformLocation(ProgramID, "M");
+	MatrixID = glGetUniformLocation(program, "MVP");
+    ViewMatrixID = glGetUniformLocation(program, "V");
+    ModelMatrixID = glGetUniformLocation(program, "M");
 
+    //Make model
+    Model monkey("assets/uvmap.DDS", "assets/suzanne.obj");
+    // monkey.setPos(glm::vec3(1.0f));
     
-    Texture = loadDDS("assets/uvmap.DDS"); //loadDDS("imgs/uvmap.DDS");
-
+    // //read .obj file
+    // std::vector<glm::vec3> vertices;
+    // std::vector<glm::vec2> uvs;
+    // std::vector<glm::vec3> normals;
+    // error = loadOBJ("assets/suzanne.obj", vertices, uvs, normals);
+    // if(error != 0)
+    // {
+    //     fprintf(stderr, "Error: Failed to load mesh.\n");
+    //     return error;
+    // }
     
-    //read .obj file
-    std::vector<glm::vec3> vertices;
-    std::vector<glm::vec2> uvs;
-    std::vector<glm::vec3> normals;
-    error = loadOBJ("assets/suzanne.obj", vertices, uvs, normals);
-    if(error != 0)
-    {
-        fprintf(stderr, "Error: Failed to load mesh.\n");
-        return error;
-    }
-    
-    //VBO indexing
-    std::vector<unsigned short> indices;
-    std::vector<glm::vec3> indexed_vertices;
-    std::vector<glm::vec2> indexed_uvs;
-    std::vector<glm::vec3> indexed_normals;
-    indexVBO(vertices, uvs, normals, indexed_vertices, indexed_uvs, indexed_normals, indices);
+    // //VBO indexing
+    // std::vector<unsigned short> indices;
+    // std::vector<glm::vec3> indexed_vertices;
+    // std::vector<glm::vec2> indexed_uvs;
+    // std::vector<glm::vec3> indexed_normals;
+    // indexVBO(vertices, uvs, normals, indexed_vertices, indexed_uvs, indexed_normals, indices);
 
     
 
     // Get a handle for  "myTextureSampler" uniform
-	TextureID  = glGetUniformLocation(ProgramID, "myTextureSampler");
-    
-    //Make and load the buffers
-    
+	TextureID  = glGetUniformLocation(program, "myTextureSampler");
+ 
 
-
-
+    //Will later be removed
     GLuint colorbuffer;  
     glGenBuffers(1, &colorbuffer);
     glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
     glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
 
-    LightID = glGetUniformLocation(ProgramID, "LightPosition_worldspace");
+    LightID = glGetUniformLocation(program, "LightPosition_worldspace");
     
     double lastTime;
     int frames;
@@ -140,15 +137,7 @@ int Game(void)
     frames = 0;
     do
     {
-        loadBuffers(
-        &vertexbuffer,
-        &uvbuffer,
-        &normalbuffer,
-        &elementbuffer,
-        indices,
-        indexed_vertices,
-        indexed_uvs,
-        indexed_normals);
+    
         double currentTime;
         currentTime = glfwGetTime();
         frames++;
@@ -159,8 +148,10 @@ int Game(void)
             lastTime += 1.0f;
         }
 
-
         glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+
+
         
         computeMatricesFromInputs();
         
@@ -170,137 +161,14 @@ int Game(void)
 
         
 	    //Use the shaders
-        glUseProgram(ProgramID);
+        glUseProgram(program);
         
         glm::vec3 lightPos = glm::vec3(4, 4, 4);
         glUniform3f(LightID, lightPos.x, lightPos.y, lightPos.y);
         //bind the contant matrix
         glUniformMatrix4fv(ViewMatrixID, 1, GL_FALSE, &ViewMatrix[0][0]);
         
-        
-        ////render first object////
-	   
-
-        glm::mat4 ModelMatrix1 = glm::mat4(1.0f);
-        glm::mat4 MVP1 = ProjectionMatrix * ViewMatrix * ModelMatrix1;
-
-
-        glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP1[0][0]);
-
-        glUniformMatrix4fv(ModelMatrixID, 1, GL_FALSE, &ModelMatrix1[0][0]);
-
-        // Bind texture in Texture Unit 0
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, Texture);
-		//Set "myTextureSampler" sampler to use Texture Unit 0
-		glUniform1i(TextureID, 0);
-
-        
-        //vertexBuffer
-        glEnableVertexAttribArray(0);
-        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-        glVertexAttribPointer(
-           0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
-           3,                  // size
-           GL_FLOAT,           // type
-           GL_FALSE,           // normalized?
-           0,                  // stride
-           (void*)0            // array buffer offset
-        );
-       
-        
-        //2nd attribute buffer: textures 
-        glEnableVertexAttribArray(1);
-        glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
-        glVertexAttribPointer(
-        1,                                // attribute. No particular reason for 1, but must match the layout in the shader.
-        2,                                // size only 2
-        GL_FLOAT,                         // type
-        GL_FALSE,                         // normalized?
-        0,                                // stride
-        (void*)0                          // array buffer offset
-        );
-
-        
-        //3rd attribute buffer: normals
-        glEnableVertexAttribArray(2);
-        glBindBuffer(GL_ARRAY_BUFFER, normalbuffer);
-        glVertexAttribPointer(
-            2,        //attribute
-            3,        //size
-            GL_FLOAT, //type
-            GL_FALSE, //normalized
-            0,        //stride
-            (void*) 0 //offset
-        );
-        
-        //Indexbuffer
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementbuffer);
-        
-        //draw the triangles
-        glDrawElements( GL_TRIANGLES, (GLsizei) indices.size(), GL_UNSIGNED_SHORT, (void*)0);
-        
-        ////render second////
-       
-        glm::mat4 ModelMatrix2 = glm::mat4(1.0f);
-        ModelMatrix2 = glm::translate(ModelMatrix2, glm::vec3(2.0f, 0.0f, 0.0f));
-        glm::mat4 MVP2 = ProjectionMatrix * ViewMatrix * ModelMatrix2;
-
-
-        glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP2[0][0]);
-
-        glUniformMatrix4fv(ModelMatrixID, 1, GL_FALSE, &ModelMatrix2[0][0]);
-
-        // Bind texture in Texture Unit 0
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, Texture);
-		//Set "myTextureSampler" sampler to use Texture Unit 0
-		glUniform1i(TextureID, 0);
-
-
-        //vertexBuffer
-        glEnableVertexAttribArray(0);
-        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-        glVertexAttribPointer(
-           0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
-           3,                  // size
-           GL_FLOAT,           // type
-           GL_FALSE,           // normalized?
-           0,                  // stride
-           (void*)0            // array buffer offset
-        );
-       
-
-        //2nd attribute buffer: textures 
-        glEnableVertexAttribArray(1);
-        glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
-        glVertexAttribPointer(
-        1,                                // attribute. No particular reason for 1, but must match the layout in the shader.
-        2,                                // size only 2
-        GL_FLOAT,                         // type
-        GL_FALSE,                         // normalized?
-        0,                                // stride
-        (void*)0                          // array buffer offset
-        );
-
-
-        //3rd attribute buffer: normals
-        glEnableVertexAttribArray(2);
-        glBindBuffer(GL_ARRAY_BUFFER, normalbuffer);
-        glVertexAttribPointer(
-            2,        //attribute
-            3,        //size
-            GL_FLOAT, //type
-            GL_FALSE, //normalized
-            0,        //stride
-            (void*) 0 //offset
-        );
-
-        //Indexbuffer
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementbuffer);
-
-        //draw the triangles
-        glDrawElements( GL_TRIANGLES, (GLsizei) indices.size(), GL_UNSIGNED_SHORT, 0);
+        render(monkey, ProjectionMatrix, ViewMatrix);
 
         
         //diable the buffers
@@ -313,6 +181,12 @@ int Game(void)
         //Update Display
         glfwSwapBuffers(window);
         glfwPollEvents();
+
+        if(error != 0)
+        {
+            fprintf(stderr, "Error: Detected at the end of the loop!\n");
+            return -1;
+        }
     } while(glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS && glfwWindowShouldClose(window) == 0);
 
     //delete and close everything
@@ -320,8 +194,7 @@ int Game(void)
 	glDeleteBuffers(1, &uvbuffer);
     glDeleteBuffers(1, &normalbuffer);
     glDeleteBuffers(1, &elementbuffer);
-	glDeleteProgram(ProgramID);
-	glDeleteTextures(1, &Texture);
+	glDeleteProgram(program);
 	glDeleteVertexArrays(1, &VertexArrayID);
 
     //terminate glfw and close the window
@@ -404,10 +277,9 @@ void loadBuffers(
     std::vector<glm::vec2> indexed_uvs,
     std::vector<glm::vec3> indexed_normals)
     {
-       glGenBuffers(1, uvbuffer);
+    glGenBuffers(1, uvbuffer);
     glBindBuffer(GL_ARRAY_BUFFER, *uvbuffer);
     glBufferData(GL_ARRAY_BUFFER, indexed_uvs.size() * sizeof(glm::vec2), &indexed_uvs[0], GL_STATIC_DRAW);
-
 
     glGenBuffers(1, vertexbuffer);
     glBindBuffer(GL_ARRAY_BUFFER, *vertexbuffer);
@@ -419,9 +291,82 @@ void loadBuffers(
     glBufferData(GL_ARRAY_BUFFER, indexed_normals.size() * sizeof(glm::vec3), &indexed_vertices[0], GL_STATIC_DRAW);
 
 
-
     glGenBuffers(1, elementbuffer);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, *elementbuffer);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW); 
-    }
+}
 
+inline int render(Model model, glm::mat4 ProjectionMatrix, glm::mat4 ViewMatrix)
+{
+    loadBuffers(
+    &vertexbuffer,
+    &uvbuffer,
+    &normalbuffer,
+    &elementbuffer,
+    model.indices,
+    model.indexed_vertices,
+    model.indexed_uvs,
+    model.indexed_normals);
+
+    ////render////
+	   
+    glm::mat4 MVP = ProjectionMatrix * ViewMatrix * model.ModelMatrix;
+
+
+    glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
+
+    glUniformMatrix4fv(ModelMatrixID, 1, GL_FALSE, &model.ModelMatrix[0][0]);
+
+    // Bind texture in Texture Unit 0
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, model.Texture);
+	//Set "myTextureSampler" sampler to use Texture Unit 0
+	glUniform1i(TextureID, 0);
+
+        
+    //vertexBuffer
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+    glVertexAttribPointer(
+    0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+    3,                  // size
+    GL_FLOAT,           // type
+    GL_FALSE,           // normalized?
+    0,                  // stride
+    (void*)0            // array buffer offset
+    );
+    
+        
+    //2nd attribute buffer: textures 
+    glEnableVertexAttribArray(1);
+    glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
+    glVertexAttribPointer(
+    1,                                // attribute. No particular reason for 1, but must match the layout in the shader.
+    2,                                // size only 2
+    GL_FLOAT,                         // type
+    GL_FALSE,                         // normalized?
+    0,                                // stride
+    (void*)0                          // array buffer offset
+    );
+
+        
+    //3rd attribute buffer: normals
+    glEnableVertexAttribArray(2);
+    glBindBuffer(GL_ARRAY_BUFFER, normalbuffer);
+    glVertexAttribPointer(
+        2,        //attribute
+        3,        //size
+        GL_FLOAT, //type
+        GL_FALSE, //normalized
+        0,        //stride
+        (void*) 0 //offset
+    );
+        
+    //Indexbuffer
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementbuffer);
+        
+    //draw the triangles
+    glDrawElements( GL_TRIANGLES, (GLsizei) model.indices.size(), GL_UNSIGNED_SHORT, (void*)0);
+        
+    return 0;
+}
---
+++ src/game/head/model.cpp
@@ -0,0 +1,37 @@
+#include "model.hpp"
+
+Model::Model(const char* texturepath, const char* objpath)
+{
+             
+     position = glm::vec3(0.0f); 
+     ModelMatrix = glm::mat4(1.0f);
+
+     //load the texture
+     error = loadDDS(texturepath, &Texture); //loadDDS("imgs/uvmap.DDS");
+
+     if(error != 0)
+     {
+          fprintf(stderr, "Error: Failed to load Texture: Exit code%d\n", error);
+          exit(error);
+     }
+
+     //read .obj file    
+     error = loadOBJ(objpath, vertices, uvs, normals);
+     if(error != 0)
+     {
+        fprintf(stderr, "Error: Failed to load mesh.\n");
+        exit(error);
+     }    
+     //VBO indexing
+     indexVBO(vertices, uvs, normals, indexed_vertices, indexed_uvs, indexed_normals, indices);
+}
+void Model::setPos(glm::vec3 position)
+{
+     this->position = position;
+     ModelMatrix = glm::translate(ModelMatrix, position);
+}
+
+Model::~Model()
+{
+     	glDeleteTextures(1, &Texture);
+}
